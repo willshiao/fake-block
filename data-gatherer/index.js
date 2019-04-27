e823a1ac9851cfec2a126f910fc5a5c3d565433d
William Shiao <willshiao@gmail.com>
2019-04-27 12:18:37 -0700

    Update model and Flask code

--- data-gatherer/index.js
+++ data-gatherer/index.js
@@ -9,24 +9,30 @@ const _ = require('lodash')
 const {createObjectCsvWriter} = require('csv-writer')
 const client = new Twitter(config.get('twitter'))
 
-
 function getClassification (row) {
   return row[1]
 }
 
 async function main () {
-  const data = fs.readFileSync('data/NLP+CSS_2016.tsv', 'utf8')
+  if (process.argv.length < 3) process.exit(1)
+  // const data = fs.readFileSync('data/NLP+CSS_2016.tsv', 'utf8')
+  const data = fs.readFileSync(process.argv[2], 'utf8')
     .split('\n')
-    .map(line => line.split('\t'))
+    .map(line => line.split(','))
+    // .map(line => line.split('\t'))
     .slice(1)
   const csvWriter = createObjectCsvWriter({
-    path: 'data/cleaned.csv',
+    path: `data/cleaned_${Date.now()}.csv`,
     header: [
-        {id: 'id', title: 'TweetID'},
-        {id: 'text', title: 'Text'},
-        {id: 'class', title: 'Class'}
+      { id: 'id', title: 'TweetID' },
+      { id: 'text', title: 'Text' },
+      { id: 'screenName', title: 'ScreenName' },
+      { id: 'favCnt', title: 'FavCount' },
+      { id: 'rtCnt', title: 'RtCount' },
+      { id: 'verified', title: 'Verified' },
+      { id: 'class', title: 'Class' }
     ]
-  });
+  })
 
   const tweetPieces = await Promise.map(_.chunk(data, 100), getTweetData, { concurrency: 3 })
   const output = Object.values(_.assign({}, ...tweetPieces))
@@ -34,7 +40,7 @@ async function main () {
   console.log('...Done!')
 }
 
-async function getTweetData(chunk) {
+async function getTweetData (chunk) {
   const classMap = {}
   const ids = []
   chunk.forEach(row => {
@@ -47,9 +53,19 @@ async function getTweetData(chunk) {
   })
 
   const tweets = await client.getAsync('statuses/lookup', { id: ids.join(',') })
+  console.log(`Getting ${tweets.length} tweets`)
   tweets.forEach(tweet => {
-    classMap[tweet.id_str].text = tweet.text
+    // console.log(tweet)
+    const storedTweet = classMap[tweet.id_str]
+    storedTweet.text = tweet.text
+    if (tweet.user) {
+      storedTweet.screenName = tweet.user.screen_name
+      storedTweet.verified = (tweet.user.verified === 'false') ? '0' : '1'
+    }
+    storedTweet.favCnt = tweet.favorite_count
+    storedTweet.rtCnt = tweet.retweet_count
   })
+
   console.log(`Got: ${tweets.length}/100 tweets`)
   for (let key in classMap) {
     if (classMap[key].text === null) {
---
+++ data-server/server.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import string
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.preprocessing import LabelEncoder
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+from flask import Flask, request
+from flask_cors import CORS
+import pickle
+
+whitelist = set(string.ascii_lowercase + string.digits)
+encoder = None
+
+# Load pickle
+with open('thepickle.pkl', 'rb') as f:
+    pickled = pickle.load(f)
+    if len(pickled) == 2:
+        vectorizer, classifier = pickled
+    else:
+        encoder, vectorizer, classifier = pickled
+        
+
+def cleanWord(word):
+    word = "".join([c for c in word.lower() if c in whitelist])
+    return word
+
+def invalidWord(word):
+    if (len(word) == 0 or word[0] == '@'):
+        return True
+    word = cleanWord(word)
+    return len(word) == 0
+
+def cleanText(text):
+    words = text.split(" ")
+    words = [cleanWord(word) for word in words if not invalidWord(word)]
+    return ' '.join(words)
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def test():
+    return 'Hello! This is a test. - Jon'
+
+@app.route('/classify', methods=['POST'])
+def classify():
+    content = request.get_json()
+    docs = [cleanText(doc['text']) for doc in content]
+    X = vectorizer.transform(docs)
+    y_hat = classifier.predict(X)
+
+    print('Get {} items'.format(len(content)))
+    for i in range(len(content)):
+        if (encoder is not None and encoder.classes_[y_hat[i]] != 'neither') or y_hat[i] == 1:
+            print('Content: {}'.format(content[i]['text']))
+            if encoder is not None:
+                print('Predicted: {}'.format(encoder.classes_[y_hat[i]]))
+            else:
+                print('Predicted: {}'.format(y_hat[i]))
+    return '{"success": true}'
