768a94192e60e53640457e32eb5bcb79793ab7b0
William Shiao <willshiao@gmail.com>
2019-04-27 04:36:36 -0700

    Improve extension

--- data-extension/content.js
+++ data-extension/content.js
@@ -1,25 +1,54 @@
 const tweetSet = new Set([])
 let lastSize = 0
-let test = true
-function jon () {
-  // console.log('?')
-  const tweetList = $('#stream-items-id>li')
-  if (tweetList.length !== lastSize) {
-    lastSize = tweetList.length
+// let test = true
 
-    tweetList.each((key, li) => {
-      const $li = $(li)
-      const id = $li.data().itemId
-      if (tweetSet.has(id)) return null
-      tweetSet.add(id)
-      if (test) {
-        $li.css('display', 'none')
-        // $li.fadeOut(3000)
-      }
-      test = !test
-    })
+function jon () {
+  const tweetList = $('#stream-items-id>li')
+  const tweets = []
+
+  if (tweetList.length === lastSize) {
+    setTimeout(jon, 200)
+    return
   }
-  setTimeout(jon, 200)
+
+  lastSize = tweetList.length
+
+  tweetList.each((key, li) => {
+    const $li = $(li)
+    const id = $li.data().itemId
+
+    if (tweetSet.has(id)) return null
+    tweetSet.add(id)
+
+    // Get text ;(
+    const text = $li.find('div.tweet div.js-tweet-text-container p').text()
+    // console.log('Got text:', text)
+    tweets.push({
+      id,
+      text
+    })
+
+    // if (test) {
+    //   $li.css('display', 'none')
+    //   // $li.fadeOut(3000)
+    // }
+    // test = !test
+  })
+  console.log('Sending: ', tweets)
+  $.ajax({
+    type: 'POST',
+    url: 'http://localhost:5000/classify',
+    data: JSON.stringify(tweets),
+    contentType: 'application/json',
+    success: (res) => {
+      console.log('Got response: ', res)
+      setTimeout(jon, 200)
+    },
+    error: (err) => {
+      console.error('Failed!!!!!', err)
+    },
+    dataType: 'json'
+  })
 }
 
 $(function () {
