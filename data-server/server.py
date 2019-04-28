be985c1d919f76aab8f8f7c3e7bfe0b94cc1149a
William Shiao <willshiao@gmail.com>
2019-04-27 23:28:06 -0700

    Update site model loading

--- data-server/server.py
+++ data-server/server.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import string
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.preprocessing import LabelEncoder
@@ -12,16 +13,25 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pickle
 
+import spacy
+
+nlp = spacy.load('en_vectors_web_lg', disable=['parser', 'tagger', 'ner', 'textcat'])
+
 whitelist = set(string.ascii_lowercase + string.digits)
 encoder = None
+vectorizer = None
 
 # Load pickle
 with open('thepickle.pkl', 'rb') as f:
     pickled = pickle.load(f)
-    if len(pickled) == 2:
-        vectorizer, classifier = pickled
+    if isinstance(pickled, list):
+        if len(pickled) == 2:
+            vectorizer, classifier = pickled
+        else:
+            encoder, vectorizer, classifier = pickled
     else:
-        encoder, vectorizer, classifier = pickled
+        classifier = pickled
+
         
 
 def cleanWord(word):
@@ -39,6 +49,17 @@ def cleanText(text):
     words = [cleanWord(word) for word in words if not invalidWord(word)]
     return ' '.join(words)
 
+def vectorizeText(text):
+    if not isinstance(text, str):
+        print('Found error: {}, {}'.format(text, type(text)))
+        text = str(text)
+    output = np.zeros(300)
+    doc = nlp(text)
+    for tok in doc:
+        output += tok.vector
+    return output
+
+
 app = Flask(__name__)
 CORS(app)
 
@@ -49,12 +70,17 @@ def test():
 @app.route('/classify', methods=['POST'])
 def classify():
     content = request.get_json()
-    docs = [cleanText(doc['text']) for doc in content]
-    X = vectorizer.transform(docs)
-    y_hat = classifier.predict(X)
 
-    print('Get {} items'.format(len(content)))
+    if vectorizer is None:
+        docs = [cleanText(doc['text']) for doc in content]
+        X = vectorizer.transform(docs)
+        y_hat = classifier.predict(X)
 
+    else:
+        X = np.array([vectorizeText(x) for x in content])
+        y_hat = classifier.predict(X)
+    
+    print('Got {} items'.format(len(content)))
     outputList = list()
     for i in range(len(content)):
         if (encoder is not None and encoder.classes_[y_hat[i]] != 'neither') or y_hat[i] == 1:
@@ -64,4 +90,5 @@ def classify():
                 print('Predicted: {}'.format(encoder.classes_[y_hat[i]]))
             else:
                 print('Predicted: {}'.format(y_hat[i]))
+
     return jsonify({ 'success': True, 'ids': outputList })
